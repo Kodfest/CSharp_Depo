e59444753f18c7ea30e81d7bfd0df78a1c9b1432
Yavuz Gedik <yavuzgedik@outlook.com>
2016-11-11 16:14:11 +0300

    Degiskenler

---
+++ Degiskenler/Degiskenler/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degisken;
+            uint degisken2;
+
+            long degisken6;
+            ulong degisken7;
+
+            bool state;
+            state = true;
+            state = false;
+
+            sbyte degisken3;
+
+            float degisken4;
+            double degisken5;
+
+            char karakter = 'a';
+            char karakter2 = '3';
+            char karakter3 = '?';
+
+            string name = "yavuz";
+            name = "semih";
+
+            var x = "yavuz";
+
+            /* 
+            
+            Aşağıdaki gibi farklı Şekillerde Tanımlama Yapılabilir
+
+            double bolum;
+            double toplam;
+            double carpim;
+            double cikan;
+
+            double bolum = 0, toplam = 1, carpim = 2, cikan = 3;
+
+            double bolum, toplam, carpim, cikan, sayi1, sayi2;
+            
+            */
+
+            //double bolum = 0, toplam = 0, carpim = 0, cikan = 0, sayi1, sayi2;
+            //Console.Write("Birinci Sayi: ");
+            //sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("İkinci Sayi: ");
+            //sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            //bolum = sayi1 / sayi2;
+            //toplam = sayi2 + sayi1;
+            //carpim = sayi1 * sayi2;
+            //cikan = sayi1 - sayi2;
+
+            //Console.WriteLine("\nToplam \t: \t" + toplam);
+            //Console.WriteLine("Çıkan \t: \t" + cikan);
+            //Console.WriteLine("Çarpım \t: \t" + carpim);
+            //Console.WriteLine("Bolum \t: \t" + bolum);
+        }
+    }
+}
