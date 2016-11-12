765e98f6c6eaaa96ecc61f7ebcd8227960427bd8
Yavuz Gedik <yavuzgedik@outlook.com>
2016-11-12 17:04:03 +0300

    For

---
+++ For/For/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("{0}. adım",i);
+            //}
+            //Console.ReadKey();
+
+            //bool durum = true;
+            //for (int i = 0; durum == true && i < 100; i = i + 5)
+            //{
+            //    if (i * 2 > 20)
+            //    {
+            //        durum = false;
+            //    }
+
+            //    Console.WriteLine("{0} iken devam ediyorum", i);
+            //}
+            //Console.ReadKey();
+
+
+            //for (int i = 0; i < 100; i = i + 5)
+            //{
+            //    if (i * 2 < 20)
+            //    {
+            //        continue;
+            //    }
+
+            //    Console.WriteLine("{0} iken devam ediyorum", i);
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
