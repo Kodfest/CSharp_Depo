9357489a7fa833275647f2dfd4b5fd15df1ed3bd
yavuzgedik <yavuzgedik@outlook.com>
2016-11-19 17:10:04 +0200

    DoWhile

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Selam");
+            } while (i != 0);
+
+            //while(i != 0)
+            //{
+            //    Console.WriteLine("Selam");
+            //}
+        }
+    }
+}
